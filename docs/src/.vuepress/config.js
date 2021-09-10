9120a82c0e0f338db2c5b2fe6989322baf22abd1
Shenna Piri <Shenna.Piri@EXXETA.com>
2021-09-10 16:28:27 +0200

    Initial commit

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,83 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'Export Company GmbH',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  theme: 'vuepress-theme-succinct',
+  globalUIComponents: [
+    'ThemeManager'
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    search: false,
+    navbar: false
+    // nav: [
+      // {
+      //   text: 'Guide',
+      //   link: '/guide/',
+      // },
+      // {
+      //   text: 'Config',
+      //   link: '/config/'
+      // },
+      // {
+      //   text: 'Email',
+      //   link: 'mailto:shennawew@outlook.com'
+      // },
+      // {
+      //   text: 'Whatsapp',
+      //   link: 'https://wa.me/4915257523364'
+      // }
+    // ],
+    // sidebar: {
+      // '/guide/': [
+      //   {
+      //     title: 'Guide',
+      //     collapsable: false,
+      //     children: [
+      //       '',
+      //       'using-vue',
+      //     ]
+      //   }
+      // ],
+    // }
+  },
+
+
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
---
+++ docs/src/index.md
@@ -0,0 +1,20 @@
+---
+home: true
+heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: 
+actionText: Products →
+actionLink: /guide/using-vue.md
+features:
+- title: Our Vision
+  details: We aim to provide our customers with best product and to be able to work together with our supplier. 
+- title: Our Product
+  details: We sell this and that product, with best quality.
+- title: About Us
+  details: We are this and this person, with an ambition to do this and that.
+footer: Shenna Risqianto Wilfred Piri
+---
+# Contact Us:
+
+[Email 📧](mailto:shennawew@outlook.com)
+
+[Whatsapp 💬](https://wa.me/4915257523364)
